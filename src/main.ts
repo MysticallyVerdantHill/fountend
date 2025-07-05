c64669be4f8286b8142dde72a7ee3c62852b6657
Hinjin-3211 <ABCDEFG9007@126.com>
2025-07-05 21:14:36 +0800

    tteesstt

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
+//尼玛
 app.use(router);
 app.use(ElementPlus);
 app.mount("#app");
